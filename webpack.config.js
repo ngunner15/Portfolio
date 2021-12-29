bfbe213519fe44e8099f9f9075f465e1ba5c0011
Nisarg Vadgama <nisargvv@gmail.com>
2021-12-29 12:55:59 -0500

    Update libraries

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const fse = require('fs-extra')
-//const webpack = require('webpack')
+const webpack = require('webpack')
 const postCSSPlugins = [
 	require('postcss-import'),
 	require('postcss-simple-vars'),
